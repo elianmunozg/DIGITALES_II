5842681060191f62b55f1eeced5f327c43c50e9a
elianmunozg <eligo2001@gmail.com>
2024-09-19 14:08:03 -0600

    Reorganizacion

--- Tarea1/calculadora.v
+++ Tarea1/calculadora.v
@@ -4,17 +4,19 @@ module calculadora (
     input [3:0] a,
     input [3:0] b,
     input [1:0] MODO,
-    output reg [3:0] c
+    output reg [3:0] c,
+    output reg rco
+    
 );
 
-//como se va comportar la salida, respecto a mis entradas
+
 
 always @(posedge clk ) begin
     if (!rst) begin //rst==0 
-        if(MODO == 2'b00) c <= a-b;
-        else if (MODO == 2'b01) c <= a+b;
-        else if (MODO == 2'b10) c <= a*b;
-        else if (MODO == 2'b11) c <= 0;
+        if(MODO == 2'b00) c <= a + b;
+        else if (MODO == 2'b01) c <= a - b;
+        else if (MODO == 2'b10) c <= a * b;
+        else if (MODO == 2'b11) c <= a << 1;
     end 
     else begin
         c <= 0;
