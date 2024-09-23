61bae6f7ae1c299cb18e00721e336a52b2d4088b
elianmunozg <eligo2001@gmail.com>
2024-09-23 09:32:10 -0600

    Implementaciones faltantes a la calculadora

--- Tarea1/calculadora.v
+++ Tarea1/calculadora.v
@@ -13,15 +13,16 @@ module calculadora (
 
 always @(posedge clk ) begin
     if (!rst) begin //rst==0 
-        if(MODO == 2'b00) c <= a + b;
-        else if (MODO == 2'b01) c <= a - b;
-        else if (MODO == 2'b10) c <= a * b;
-        else if (MODO == 2'b11) c <= a << 1;
+        if(MODO == 2'b00) {rco, c} <= a + b;
+        else if (MODO == 2'b01) {rco, c} <= a - b;
+        else if (MODO == 2'b10) {rco, c} <= a * b;
+        else if (MODO == 2'b11) {rco, c} <= a << 1;
     end 
     else begin
         c <= 0;
+        rco <=0;
     end
-    $display("HOLA MUNDO c %b", c);
+
 end
 
 
--- Tarea1/test.v
+++ Tarea1/test.v
@@ -6,9 +6,9 @@ module test (
     output reg [3:0] a,
     output reg [3:0] b,
     output reg [1:0] MODO,
-    input [3:0] c
+    input [3:0] c,
+    input rco
 );
-
 
 always begin
     #5 clk = !clk;
@@ -16,15 +16,46 @@ end
 
 initial begin
     clk = 0;
-    rst = 1; //yo quiero comenzar reseteando mi calculadora
+    rst = 1;
     #10;
     rst = 0;
+
+    MODO = 2'b00;
+    a = 4'b0010;
+    b = 4'b0011;
+    #20;
+
+    a = 4'b1111;
+    b = 4'b0001;
+    #20;
+
     MODO = 2'b01;
-    a = 1; // a = 4'b1111
-    b = 1; // b = 4'b1111
-    #30;
+    a = 4'b0100;
+    b = 4'b0010;
+    #20;
+
+    a = 4'b0010;
+    b = 4'b0110;
+    #20;
+
+    MODO = 2'b10;
+    a = 4'b0010;
+    b = 4'b0010;
+    #20;
+
+    a = 4'b1110;
+    b = 4'b0010;
+    #20;
+
+    MODO = 2'b11;
+    a = 4'b0010;
+    b = 0;
+    #20;
+
+    a = 4'b1100;
+    #20;
+
     $finish;
 end
 
-    
 endmodule
